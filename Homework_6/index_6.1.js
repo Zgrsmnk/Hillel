8b67bd6271d708710b4443d84e6f60c77c0127a3
Yevhenii Herasymenko <zhenya36546@gmail.com>
2026-03-10 17:49:27 +0200

    finished first exercise

---
+++ Homework_6/index_6.1.js
@@ -0,0 +1,15 @@
+let string = prompt("Please enter a string");
+let charakters = prompt("Please enter a charakters to remove");
+
+function removeChars(string, charakters) {
+    let result = '';
+
+    for (let i = 0; i < string.length; i++) {
+        if (!charakters.includes(string[i])) {
+            result += string[i];
+        }
+    }
+    return result;
+}
+
+console.log(removeChars(string, charakters));
