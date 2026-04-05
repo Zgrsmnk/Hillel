785c9c86a55f77e21c7fe7168a836cd69241b5da
Yevhenii Herasymenko <zhenya36546@gmail.com>
2026-04-05 09:38:02 +0300

    fixed code

--- Homework_13/index.js
+++ Homework_13/index.js
@@ -1,53 +1,43 @@
 const form = document.querySelector("form");
 
-const inputName = document.querySelector("input[name='name']");
-const inputMessage = document.querySelector("textarea[name='message']");
-const inputPhone = document.querySelector("input[name='phone']");
-const inputEmail = document.querySelector("input[name='e-mail']");
-
-const errorName = document.querySelector(".error_name");
-const errorMessage = document.querySelector(".error_message");
-const errorPhone = document.querySelector(".error_number");
-const errorEmail = document.querySelector(".error_email");
-
-const nameReg = /^[a-zA-ZА-Я]{2,}$/i;
-const messageReg = /^.{5,}$/;
-const numberReg = /^\+380\d{9}$/;
-const emailReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const fields = [
+  {
+    input: document.querySelector("input[name='name']"),
+    error: document.querySelector(".error_name"),
+    reg: /^[a-zA-ZА-Я]{2,}$/i,
+  },
+  {
+    input: document.querySelector("textarea[name='message']"),
+    error: document.querySelector(".error_message"),
+    reg: /^.{5,}$/,
+  },
+  {
+    input: document.querySelector("input[name='phone']"),
+    error: document.querySelector(".error_number"),
+    reg: /^\+380\d{9}$/,
+  },
+  {
+    input: document.querySelector("input[name='e-mail']"),
+    error: document.querySelector(".error_email"),
+    reg: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+];
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let isValid = true;
 
-  if (!nameReg.test(inputName.value)) {
-    errorName.classList.add("error_visible");
-    isValid = false;
-  } else {
-    errorName.classList.remove("error_visible");
-  }
+  fields.forEach(function (field) {
+    const valid = field.reg.test(field.input.value);
 
-  if (!messageReg.test(inputMessage.value)) {
-    errorMessage.classList.add("error_visible");
-    isValid = false;
-  } else {
-    errorMessage.classList.remove("error_visible");
-  }
-
-  if (!numberReg.test(inputPhone.value)) {
-    errorPhone.classList.add("error_visible");
-    isValid = false;
-  } else {
-    errorPhone.classList.remove("error_visible");
-  }
-
-  if (!emailReg.test(inputEmail.value)) {
-    errorEmail.classList.add("error_visible");
-    isValid = false;
-  } else {
-    errorEmail.classList.remove("error_visible");
-  }
-
+    if (!valid) {
+      field.error.classList.add("error_visible");
+      isValid = false;
+    } else {
+      field.error.classList.remove("error_visible");
+    }
+  });
   if (isValid) {
     const formData = new FormData(event.target);
     const formObj = {};
