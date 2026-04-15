7c1580bed8e3e496fa122d29673a37e926fdcc77
Yevhenii Herasymenko <zhenya36546@gmail.com>
2026-04-15 03:02:08 +0300

    finished work

--- Homework_16/index.js
+++ Homework_16/index.js
@@ -3,7 +3,8 @@ function Student(first, last, year) {
   this.lastName = last;
   this.userAge = year;
 
-  studentGrade = new Array(25);
+  this.studentAttendance = [];
+  this.grades = [];
 
   Object.defineProperties(this, {
     age: {
@@ -14,10 +15,112 @@ function Student(first, last, year) {
     },
   });
 
-  this.present = function () {};
+  this.present = function () {
+    const presentEl = true;
+    if (this.studentAttendance.length < 25) {
+      this.studentAttendance.push(presentEl);
+    }
+  };
 
-  this.absent = function () {};
+  this.absent = function () {
+    const absentEl = false;
+    if (this.studentAttendance.length < 25) {
+      this.studentAttendance.push(absentEl);
+    }
+  };
+
+  this.getGrade = function () {
+    const sum = this.grades.reduce(function (accumulator, currentValue) {
+      return accumulator + currentValue;
+    }, 0);
+
+    return sum / this.grades.length;
+  };
+
+  this.getAttendance = function () {
+    const getTrue = this.studentAttendance.filter(function (element) {
+      return element === true;
+    });
+    return getTrue.length / this.studentAttendance.length;
+  };
+
+  this.summary = function () {
+    if (this.getGrade() > 90 && this.getAttendance() >= 0.9) {
+      return "Молодець";
+    } else if (this.getGrade() > 90 || this.getAttendance() > 0.9) {
+      return "Добре, але можна краще";
+    } else {
+      return "Редиска";
+    }
+  };
 }
 
-const result = new Student("john", "Vazovski", new Date(2003, 2, 13));
-console.log(`Name: ${result.firstName} ${result.lastName}, Age: ${result.age}`);
+// student 1
+const studentOne = new Student("John", "Vazovski", new Date(2003, 2, 13));
+
+studentOne.grades.push(100, 95, 98, 97, 96, 99, 100, 94, 98, 97);
+
+studentOne.present();
+studentOne.present();
+studentOne.absent();
+studentOne.present();
+studentOne.present();
+studentOne.present();
+studentOne.present();
+studentOne.present();
+studentOne.present();
+studentOne.present();
+
+console.log(
+  `Name: ${studentOne.firstName} ${studentOne.lastName},
+    Age: ${studentOne.age},
+    Result: ${studentOne.summary()}
+   `,
+);
+
+// student 2
+const studentTwo = new Student("Tolia", "Nazarov", new Date(1996, 9, 3));
+
+studentTwo.grades.push(85, 88, 90, 82, 87, 89, 91, 84, 86, 88);
+
+studentTwo.present();
+studentTwo.present();
+studentTwo.present();
+studentTwo.present();
+studentTwo.present();
+studentTwo.present();
+studentTwo.present();
+studentTwo.present();
+studentTwo.present();
+studentTwo.present();
+
+console.log(
+  `Name: ${studentTwo.firstName} ${studentTwo.lastName},
+    Age: ${studentTwo.age},
+    Result: ${studentTwo.summary()}
+   `,
+);
+
+// student 3
+const studentThree = new Student("Oleg", "Kozak", new Date(2000, 7, 11));
+
+studentThree.grades.push(50, 60, 55, 58, 52, 61, 57, 54, 59, 53);
+
+studentThree.present();
+studentThree.absent();
+studentThree.absent();
+studentThree.present();
+studentThree.absent();
+studentThree.absent();
+studentThree.absent();
+studentThree.absent();
+studentThree.absent();
+studentThree.present();
+
+console.log(
+  `Name: ${studentThree.firstName} ${studentThree.lastName},
+    Age: ${studentThree.age},
+    Result: ${studentThree.summary()}
+   `,
+);
+
