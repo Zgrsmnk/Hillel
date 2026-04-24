b671b929fa80d3e7fed39e4815d5115451cd56bd
Yevhenii Herasymenko <zhenya36546@gmail.com>
2026-04-25 02:05:33 +0300

    in process

---
+++ Homework_19/index.js
@@ -0,0 +1,24 @@
+function getWeather() {
+  const weatherResponse = fetch(
+    "https://api.openweathermap.org/data/2.5/weather?lat=50.4473092&lon=30.5227896&appid=60a6e85ae014194d7b2597e684009bc0&units=metric&lang=ua",
+  );
+
+  weatherResponse
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      const city = document.querySelector(".city");
+      const temp = document.querySelector(".temp");
+      const desc = document.querySelector(".description");
+      const wind = document.querySelector(".wind");
+      const icon = document.querySelector(".icon");
+
+      city.textContent = result.name;
+      temp.textContent = `Temp: ${Math.round(result.main.temp)}°C`;
+      desc.textContent = result.weather[0].description;
+      wind.textContent = `Wind: ${result.wind.speed}m/s`;
+      icon.src = "https://openweathermap.org/img/wn/01d@2x.png";
+    });
+}
+getWeather();
